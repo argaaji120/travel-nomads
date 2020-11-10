23a143c3a5d0e1d1beb9fc8a507ba92f0de17ad5
argaaji120 <55268287+argaaji120@users.noreply.github.com>
2020-11-10 16:11:10 +0700

    integrasi dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use App\Models\TravelPackage;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('pages.admin.dashboard');
+        $travel_package = TravelPackage::count();
+        $transaction    = Transaction::count();
+        $transaction_success = Transaction::where('transaction_status', 'SUCCESS')->count();
+        $transaction_pending = Transaction::where('transaction_status', 'PENDING')->count();
+
+        return view('pages.admin.dashboard', compact(['travel_package', 'transaction', 'transaction_success', 'transaction_pending']));
     }
 }
